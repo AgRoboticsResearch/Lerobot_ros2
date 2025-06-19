44c8f4b34f6a29f890aa5b20a89191b2aed9dab1
forgetwhatuwant <forgetwhatuwant@gmail.com>
2025-06-19 17:30:36 +0800

    Initial commit: Add SO101 follower arm ROS2 packages with description, moveit config, hardware interface, and topic-based control

---
+++ src/so101_hw_interface/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from setuptools import find_packages
+import os
+from glob import glob
+
+package_name = 'so101_hw_interface'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(include=[package_name, f"{package_name}.*", 'lerobot', 'lerobot.*']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'resource'), glob('resource/*.xml')),
+        # Install wrapper scripts into lib/<package_name> so ros2 run can locate them
+        (os.path.join('lib', package_name), ['scripts/so101_motor_bridge', 'scripts/so101_calibrate']),
+        ('share/ament_index/resource_index/executables', ['resource/so101_motor_bridge', 'resource/so101_calibrate']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hls',
+    maintainer_email='hls@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    entry_points={
+        'console_scripts': [
+            'so101_motor_bridge = so101_hw_interface.motor_bridge:main',
+            'so101_calibrate = so101_hw_interface.calibrate_arm:main',
+        ],
+    },
+) 
